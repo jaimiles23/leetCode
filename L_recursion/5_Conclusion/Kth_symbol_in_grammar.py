f4c0564deba7cf3e0c188eeb2585abb610462809
Jai <jaimiles23@gmail.com>
2019-11-26 20:37:45 -0800

    L_recusion: added .md writeup for kth symbol grammar explanation

--- L_recursion/5_Conclusion/Kth_symbol_in_grammar.py
+++ L_recursion/5_Conclusion/Kth_symbol_in_grammar.py
@@ -53,8 +53,6 @@ As such, I will use these heuristics to create the recursive solution below.
 ### LeetCode Diagnostic:
 Runtime: 20 ms, faster than 99.11% of Python3 online submissions for K-th Symbol in Grammar.
 Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for K-th Symbol in Grammar.
-
-
  ]
  */
 """

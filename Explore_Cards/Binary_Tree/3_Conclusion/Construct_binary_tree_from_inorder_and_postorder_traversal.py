77ab9a7800ea37b5e4e8750bb75a46748296dfe3
Jai <jaimiles23@gmail.com>
2019-12-05 14:27:34 -0800

    documented derived algorithm

--- Explore_Cards/Binary_Tree/3_Conclusion/Construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ Explore_Cards/Binary_Tree/3_Conclusion/Construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -3,14 +3,26 @@
  * @author [Jai Miles]
  * @email [jaimiles23@gmail.com]
  * @create date 2019-12-04 18:53:53
- * @modify date 2019-12-04 18:54:00
+ * @modify date 2019-12-05 14:24:43
  * @desc [
 Contains solutions to leetCode's [106. Construct Binary Tree from Inorder and Postorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/_)
 
 # Construct Binary Tree from Inorder and Postorder Traversal
+I was unfamiliar with constructing binary trees from inorder and post order traversal. To solve this problem I
+wrote out test cases on a piece of paper and assigned each index a representation of where it was on the tree. 
+From this assignment, I derived the following patterns:
+1. postOrder[-1] = root
+2. postOrder[-2] = root.right
+3. inOrder[inOrder.index(root) - 1] == root.left
 
+I believe that we can follow this same pattern to re-create the tree. For each node:
+    - node.right = postOrder[node - 1]
+    - node.left = inOrder[inOrder.index(node) - 1]
 
-
+To implement these two principles, I will use an iterative algorithm. The algorithm will modify the
+inOrder and postOrder lists to be tuples of two values (node, visited). The visited variable will be a 
+binary indicator if the node has been used. If the node adjacent to a visited node is used, then that node 
+will be considered a leaf (ending) on that adjacent side.
 ]
  */
 """

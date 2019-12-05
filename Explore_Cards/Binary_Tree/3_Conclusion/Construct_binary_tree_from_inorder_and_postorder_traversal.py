d14a12aeda68ae4d77b58b782a51a5340c4596d7
Jai <jaimiles23@gmail.com>
2019-12-04 19:05:42 -0800

    added unit tests

--- Explore_Cards/Binary_Tree/3_Conclusion/Construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ Explore_Cards/Binary_Tree/3_Conclusion/Construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -5,11 +5,41 @@
  * @create date 2019-12-04 18:53:53
  * @modify date 2019-12-04 18:54:00
  * @desc [
-Contains solutions to leetCode's [106. Construct BInary Tree from Inorder and Postorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/_)
+Contains solutions to leetCode's [106. Construct Binary Tree from Inorder and Postorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/_)
 
-# Construct BInary Tree from Inorder and Postorder Traversal
+# Construct Binary Tree from Inorder and Postorder Traversal
 
 
- ]
+
+]
  */
 """
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x, left = None, right = None):
+        self.val = x
+        self.left = left
+        self.right = right
+
+
+def unit_tests():
+    tester = None
+
+    print('test 1')
+    inorder = [9, 3, 15, 20, 7]
+    postorder = [9, 15, 7, 20, 3]
+    output = TreeNode(3,
+                TreeNode(9),
+                TreeNode(20,
+                    TreeNode(15),
+                    TreeNode(7))
+    )
+    assert tester.buildTree(inorder, postorder) == output
+
+
+def main():
+    unit_tests()
+
+
+if __name__ == '__main__':
+    main()

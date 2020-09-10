ec468bebdcdbf6181e2405d93c4d48bf42e9e7b3
Jai <jaimiles23@gmail.com>
2020-09-09 19:29:34 -0700

    added 2 solutions with stats on runtime & memory performance

--- Problems/1480_running_sum_of_array.py
+++ Problems/1480_running_sum_of_array.py
@@ -6,6 +6,18 @@
  * @desc [
      Solution to running sum of array:
         https://leetcode.com/problems/running-sum-of-1d-array/
+
+Solution 1 - simple for loop iterating over list
+    - Runtime: 40, 44 ms
+    - Memory: 13.9, 14 MB
+
+Solution 2 - Generator function to yield results
+    - Runtime: 36, 40 ms
+    - Memory: 14.1, 14.2 MB
+
+Notes:
+- both solutions have a time complexity of O(N).
+
  ]
  */
 """
